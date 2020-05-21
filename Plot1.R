7b692c45483b12be9255b887fcbb05ae671c0198
madneuf <63815945+madneuf@users.noreply.github.com>
2020-05-21 16:45:01 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -13,12 +13,12 @@ names(powerdata)
 
 powerdata[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 powerdata[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
-powerdate <- powerdata <- powerdata[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+powerDT <- powerdata <- powerdata[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
 png("plot1.png", width=480, height=480)
 
 ## Plot 1
-hist(powerdata[, Global_active_power], main="Global Active Power", 
+hist(powerDT[, Global_active_power], main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 dev.off()
